6c02dbb2db1d4a81341aa162b1b9a9d0b5995f39
Sanchit Kapoor <sanchitlucknow@gmail.com>
2016-04-05 02:23:35 +0530

    Testing gulp-concat, gulp-rename, and gulp-uglify. Gulp rocks

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,19 @@
 var gulp = require('gulp');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
 
 gulp.task('default', function() {
 	console.log("Running the default task!");
 })
+
+gulp.task('scripts', function() {
+	gulp.src('src/**/*.js')
+		.pipe(concat('struct.js'))
+		.pipe(gulp.dest('lib/'))
+		.pipe(uglify())
+		.pipe(rename('struct.min.js'))
+		.pipe(gulp.dest('lib/'));
+});
+
+gulp.task('automate', ['scripts']);
